f924589f9e2149148fd03501475c09c5b778bd71
Andrew <86121252+SlowsieNT@users.noreply.github.com>
2022-07-12 15:06:54 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-// npm install: ws
-// npm install: socks-proxy-agent
-// usage: socket = CreateTorWebSocket("etc.onion", 1111)
-function CreateTorWebSocket(aOnionHostname, aOnionPort) {
-	// this is "Client-side", yes 
-	var WebSocket = require('ws'),
-		SocksProxyAgent = require('socks-proxy-agent');
+// npm install ws
+// npm install socks-proxy-agent
+// Usage: socket = CreateTorWebSocket("etc.onion", 1111)
+var WebSocket2 = require('ws'),
+	SocksProxyAgent = require('socks-proxy-agent').SocksProxyAgent;
+function CreateTorWebSocket(aOnionHostname, aOnionPort, aScheme, aTorSocksPort) {
+	// Or use 9150:
+	aTorSocksPort = aTorSocksPort||9050;
+	aScheme = aScheme||"ws";
 	// WebSocket endpoint for the proxy to connect to
-	var vEndPoint = 'ws://' + aOnionHostname + ':' + aOnionPort;
+	var vEndPoint = aScheme + '://' + aOnionHostname + ':' + aOnionPort;
 	// TOR Socks5 URI:
-	var vSocks5Agent = new SocksProxyAgent("socks://127.0.0.1:9050");
+	var vSocks5Agent = new SocksProxyAgent({hostname:"127.0.0.1", port:aTorSocksPort});
 	// Return WebSocket handle
-	return new WebSocket(vEndPoint, { agent: vSocks5Agent });
+	return new WebSocket2(vEndPoint, { agent: vSocks5Agent });
 }
-
-var host = "aaaaaaaaaaaaaaaaaaaaaaaaaaa.onion";
-var port = 1502;
-var socket = CreateTorWebSocket(host, port);
-
-socket.on('open', function () {
-	console.log('"open" event!');
-	socket.send('ping');
-});
-
-socket.on('message', function (data, flags) {
-	console.log('received %j %j', data, flags);
-	//socket.close();
-});
+if ("undefined" !== typeof module)
+	module.exports = CreateTorWebSocket;
